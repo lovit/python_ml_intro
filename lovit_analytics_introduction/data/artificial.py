f3254dce1fd8cbb5bedcb8138f88a6b498304dbc
lovit <soy.lovit@gmail.com>
2019-11-09 22:46:01 +0900

    Availble to set random seed

--- lovit_analytics_introduction/data/artificial.py
+++ lovit_analytics_introduction/data/artificial.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def make_linear_regression_data(n_data=100, a=1.0, b=1.0, noise=1.0, x_range=(-10.0, 10.0)):
+def make_linear_regression_data(n_data=100, a=1.0, b=1.0,
+    noise=1.0, x_range=(-10.0, 10.0), random_seed=None):
     """
     It generates artificial data for linear regression
 
@@ -9,18 +10,23 @@ def make_linear_regression_data(n_data=100, a=1.0, b=1.0, noise=1.0, x_range=(-1
     :param float b: Interpret coefficient b in 'y = ax + b'
     :param float noise: Range of residual, e = y - (ax + b)
     :param tuple x_range: size = (float, float)
+    :param int_or_None random_seed: If not None, fix random seed
+
     :returns: x, y
         - x : numpy.ndarray, shape = (n_data,)
         - y : numpy.ndarray, shape = (n_data,)
 
     Usage
 
-        >>> x, y = make_linear_regression()
-        >>> x, y = make_linear_regression(
+        >>> x, y = make_linear_regression_data()
+        >>> x, y = make_linear_regression_data(
             n_data=100, a=1.0, b=1.0, noise=1.0, x_range=(-10.0, 10.0))
     """
 
     assert (len(x_range) == 2) and (x_range[0] < x_range[1])
+
+    if isinstance(random_seed, int):
+        np.random.seed(random_seed)
 
     x_scale = x_range[1] - x_range[0]
     x = np.random.random_sample(n_data) * x_scale + x_range[0]
