c5e31f856201e246edcd166aa3e42de742705997
lovit <soy.lovit@gmail.com>
2020-02-13 00:56:33 +0900

    Upload classifier activation map drawing tools

--- mydata/visualizer/__init__.py
+++ mydata/visualizer/__init__.py
@@ -1,3 +1,5 @@
+from ._bokeh import draw_activate_image
+from ._bokeh import scatterplot_2class
 from .bokeh import scatterplot
 from .bokeh import overlay_regression_line
 from .display import bokeh_image_slider
